21524fa0542194c5caba429dc00346b3f2e32757
Alex Telegin <telegin.alexander@gmail.com>
2022-01-16 18:49:37 +0400

    Initial commit

---
+++ contracts/Flipper.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.11;
+
+import "hardhat/console.sol";
+
+contract Flipper {
+    constructor(string memory _greeting) {
+    }
+}
---
+++ test/SolvexFlipper.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Flipper", function () {
+  it("", async function () {
+    const Greeter = await ethers.getContractFactory("Flipper");
+  });
+});
